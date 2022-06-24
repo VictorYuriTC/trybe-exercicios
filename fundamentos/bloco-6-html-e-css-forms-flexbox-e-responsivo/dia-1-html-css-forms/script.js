b6d6312f8767be5a77c134f3fc8c5433b9fe0ab1
Victor Yuri <victoryuritc@yahoo.com>
2022-06-24 03:47:57 -0300

    using disabled with bang, since the checked value is always the opposite of the disabled value. accessing inputs values with input.value.length. returning alerts when necessary, in order to avoid repeated alerts. showing an isolated alert when everything is okay. min and max characters function added

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,14 +1,7 @@
-const buttonClearInfo = document.getElementById('button-clear-info');
+/* 3. Interrompa o comportamento padrão do botão submit utilizando o método preventDefault(). Nossa amiga
+Carol Silva nos contou um pouco sobre como fazer isso, lembra? */
+
 const buttonSubmitInfo = document.getElementById('button-submit-info');
-const form = document.getElementById('form');
-
-const clearForms = () => {
-  buttonClearInfo.addEventListener('click', () => {
-    form.reset();
-  })
-}
-
-clearForms();
 
 const preventSubmitAction = () => {
   buttonSubmitInfo.addEventListener('click', (event) => {
@@ -17,3 +10,53 @@ const preventSubmitAction = () => {
 }
 
 preventSubmitAction();
+
+/* 4. Crie um botão que limpe as informações contidas nos campos; */
+
+const buttonClearInfo = document.getElementById('button-clear-info');
+const form = document.getElementById('form');
+
+const clearForms = () => {
+  buttonClearInfo.addEventListener('click', () => {
+    form.reset();
+  });
+}
+
+clearForms();
+
+/* Bônus. 1. A TrybeTrip precisa muito de fotos para divulgar seus concursos. Tendo isso em mente, faça com que
+somente quem autorizar o uso de imagens possa enviar suas informações. */
+
+const userAgreement = document.getElementById('user-agreement');
+buttonSubmitInfo.disabled = true
+
+const authorizeSubmitting = () => {
+  userAgreement.addEventListener('click', () => {
+    buttonSubmitInfo.disabled = !userAgreement.checked;
+  });
+}
+
+authorizeSubmitting();
+
+/* 2. Faça a validação dos campos com limite de caracteres. Caso não estejam dentro do esperado ao clicar
+no botão de submit, um alerta deve ser mostrado com a mensagem: 'Dados Inválidos'. Caso contrário, a
+mensagem 'Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip.' deverá aparecer na tela. */
+
+const userFullName = document.getElementById('user-full-name');
+const userEmail = document.getElementById('user-email');
+const userReason = document.getElementById('user-reason');
+
+const maxCharacters = () => {
+  buttonSubmitInfo.addEventListener('click', () => {
+    const nameLength = userFullName.value.length;
+  const emailLength = userEmail.value.length;
+  const reasonLength = userReason.value.length;
+  const invalid = 'Dados inválidos';
+    if (emailLength < 10 || emailLength > 50) { return alert(invalid) };
+    if (nameLength < 10 || nameLength > 40) { return alert(invalid) };
+    if (reasonLength > 500) { return alert(invalid) };
+    alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip');
+  });
+}
+
+maxCharacters();
