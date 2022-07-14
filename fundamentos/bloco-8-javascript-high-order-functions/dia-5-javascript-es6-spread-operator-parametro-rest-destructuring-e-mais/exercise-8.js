584ff38f55bca9ce2a40f2a8877ec8a20719a8a8
Victor Yuri <victoryuritc@yahoo.com>
2022-07-14 16:29:51 -0300

    default parameter alternative at exercise 8

--- fundamentos/bloco-8-javascript-high-order-functions/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-8.js
+++ fundamentos/bloco-8-javascript-high-order-functions/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise-8.js
@@ -1,10 +1,18 @@
 /* 8 - Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento: */
 
-const greet = (personName, selectedGreeting) => {
+const greet = (personName, selectedGreeting = 'hi') => {
+  return `${selectedGreeting} ${personName}`;
+}
+
+const greetAlternative = (personName, selectedGreeting) => {
   if (!selectedGreeting) { selectedGreeting = 'Hi'};
-  return `${selectedGreeting} ${personName}`
-};
+  return `${selectedGreeting} ${personName}`;
+}
 
 console.log(greet('John')) // 'Hi John'
 console.log(greet('John', 'Good morning')) // 'Good morning John'
 console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
+
+console.log(greetAlternative('John'));
+console.log(greetAlternative('John', 'Good morning'));
+console.log(greetAlternative('Isabela', 'Oi'));
